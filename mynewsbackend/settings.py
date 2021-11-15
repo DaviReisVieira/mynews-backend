7b64b83ba50964ab9830752cc51eb5d294c34260
DaviReisVieira <davirvs@al.insper.edu.br>
2021-11-15 14:36:27 -0300

    ENV settings

--- mynewsbackend/settings.py
+++ mynewsbackend/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = "django-insecure-&84x%opcwg9g2mcn%o#-2pn2u@v@fw81xxfn(r2+x5-i_jb^o("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
